fe2c8ae83cb555f1804840258811df4e443aedf1
daniels145 <serranodaniel195@gmail.com>
2025-06-21 15:54:53 +0100

    productos add, update, delete

--- src/main/java/pt/ipleiria/estg/dei/ei/esoft/models/Product.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/esoft/models/Product.java
@@ -7,13 +7,16 @@ public class Product {
     private String category; // combo, bebida, snack
     private double price;
     private String imagePath;
+    private Integer quantity;
 
-    public Product(String name, String description, String category, double price, String imagePath) {
+    // Constructor nuevo (con quantity)
+    public Product(String name, String description, String category, double price, String imagePath, Integer quantity) {
         this.name = name;
         this.price = price;
         this.description = description;
         this.category = category;
         this.imagePath = imagePath;
+        this.quantity = quantity;
     }
 
     public String getPhoto() {
@@ -35,4 +38,36 @@ public class Product {
     public String getCategory() {
         return category;
     }
+
+    public Integer getQuantity() {return quantity;}
+
+    public String getImagePath() {return imagePath;}
+
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+
+
 }
